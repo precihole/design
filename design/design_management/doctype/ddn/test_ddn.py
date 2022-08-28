c5f0370dbdef62b02fdf0a60a2a629e8fa8b8361
midhat@2020 <midhat@preciholesports.com>
2022-08-28 10:51:11 +0530

    First commit

---
+++ design/design_management/doctype/ddn/test_ddn.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Rehan Ansari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDDN(FrappeTestCase):
+	pass
---
+++ design/design_management/doctype/design_printout_creation/design_printout_creation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Rehan Ansari and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Design Printout Creation', {
+	// refresh: function(frm) {
+
+	// }
+});
