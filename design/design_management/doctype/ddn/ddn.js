3f08e6bdfc3c4d70b4f96d869e3b61fcf3e55f09
rehanansari389 <rehan@preciholesports.com>
2022-08-29 11:18:13 +0530

    report and chart updated

--- design/design_management/doctype/ddn/ddn.js
+++ design/design_management/doctype/ddn/ddn.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('DDN', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if(frm.doc.docstatus == 1){
+			frm.add_custom_button(__('Stock Ledger'), function() {
+				frappe.route_options = {
+					"voucher_no": frm.doc.name,
+					"from_date":frm.doc.posting_date,
+					"to_date":frm.doc.posting_date
+				};
+				frappe.set_route("query-report", "Design Ledger");
+			}, __("View"));
+		}
+	}
 });
