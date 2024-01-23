89e337116679dc4bad4b89f4896a7f0b8b010dbb
precihole <111174201+precihole@users.noreply.github.com>
2024-01-23 15:47:52 +0530

    Update drawing_permission.py

--- design/design_management/doctype/drawing_permission/drawing_permission.py
+++ design/design_management/doctype/drawing_permission/drawing_permission.py
@@ -6,7 +6,7 @@ from frappe.model.document import Document
 
 class DrawingPermission(Document):
 	def before_save(self):
-		self.file_url = frappe.utils.get_url() + self.file_url
 		if not doc.item_code:
 			return
-		frappe.db.get_value('File', {'attached_to_doctype': 'Item', 'attached_to_name': doc.item_code}, ['file_url'])
+		file_url = frappe.db.get_value('File', {'attached_to_doctype': 'Item', 'attached_to_name': doc.item_code}, ['file_url'])
+		self.file_url = frappe.utils.get_url() + file_url
