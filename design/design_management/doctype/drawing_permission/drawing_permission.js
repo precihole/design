6bb159704df2fa356f46f91e9a5f760f4c12f034
erpadmin@preciholesports.com <erpadmin@preciholesports.com>
2024-01-24 13:28:48 +0530

    fix: form layout and funtionality

--- design/design_management/doctype/drawing_permission/drawing_permission.js
+++ design/design_management/doctype/drawing_permission/drawing_permission.js
@@ -2,7 +2,36 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Drawing Permission', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.item_code && !frm.doc.file_url){
+			frm.add_custom_button(__('Fetch File'), function () {
+				frappe.call({
+					method: "design.design_management.doctype.drawing_permission.drawing_permission.get_drawing_file",
+					args: {
+						"item_code": frm.doc.item_code
+					},
+					callback: function (res) {
+						if (res.message) {
+							frm.set_value('file_url', res.message)
+							frm.set_value('status', 'Ready to Share')
+							frm.save()
+						}
+						else{
+							frappe.msgprint('File Not Found')
+						}
+					}
+				})
+			});
+		}
+	},
+	to_date: function(frm) {
+		if (!frm.doc.from_date){
+			frm.set_value('to_date', undefined)
+			frappe.msgprint('Enter from date first')
+		}
+		if (frm.doc.to_date && frm.doc.to_date < frm.doc.from_date){
+			frm.set_value('to_date', undefined)
+			frappe.msgprint('To Date should be greater than from date')
+		}
+	}
 });
--- design/design_management/doctype/drawing_permission/drawing_permission.py
+++ design/design_management/doctype/drawing_permission/drawing_permission.py
@@ -5,11 +5,32 @@ import frappe
 from frappe.model.document import Document
 
 class DrawingPermission(Document):
-	def before_save(self):
+	def before_insert(self):
 		if not self.item_code:
 			return
 		file_url = frappe.db.get_value('File', {'attached_to_doctype': 'Item', 'attached_to_name': self.item_code}, ['file_url'])
 		if file_url:
 			self.file_url = file_url
+			self.status = 'Ready to Share'
 		else:
+			self.status = 'Draft'
 			frappe.msgprint('File Not Found')
+	
+	def before_save(self):
+		if self.item_code and self.file_url:
+			if not self.from_date and self.to_date:
+				frappe.throw('From date is required')
+			elif not self.to_date and self.from_date:
+				frappe.throw('To date is required')
+				
+	def before_submit(self):
+		if self.file_url and not self.from_date and not self.to_date:
+			self.status = 'Shared'
+		elif self.file_url and self.from_date and self.to_date:
+			self.status = 'Time-Limited Shared'
+
+@frappe.whitelist()	
+def get_drawing_file(item_code):
+	file_url = frappe.db.get_value('File', {'attached_to_doctype': 'Item', 'attached_to_name': item_code}, ['file_url'])
+	if file_url:
+		return file_url
